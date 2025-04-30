9c007c0e2c79990d3f210078961d52eb747cefa7
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2025-04-30 23:04:33 +0100

    test using lazygit

---
+++ src/main/java/com/durgesh/hotel/config/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package com.durgesh.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl("jdbc:mariadb://localhost:3308/hotels");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+}
